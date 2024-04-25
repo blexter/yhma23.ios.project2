90758a379754578f8a6436273d921433aac5d672
Andreas Selguson <github@selguson.se>
2024-04-25 09:36:47 +0200

    Initial Commit

---
+++ yhma23.ios.project2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yhma23.ios.project2
+//
+//  Created by Andreas Selguson on 2024-04-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yhma23.ios.project2/yhma23_ios_project2App.swift
@@ -0,0 +1,17 @@
+//
+//  yhma23_ios_project2App.swift
+//  yhma23.ios.project2
+//
+//  Created by Andreas Selguson on 2024-04-25.
+//
+
+import SwiftUI
+
+@main
+struct yhma23_ios_project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
